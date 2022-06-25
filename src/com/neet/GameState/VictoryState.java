950703f31ec7abf0abe0b7377df02b84af40cc1a
NgDucLoc <ngducloc1112002@gmail.com>
2022-06-26 01:30:30 +0700

    done

--- src/com/neet/GameState/VictoryState.java
+++ src/com/neet/GameState/VictoryState.java
@@ -3,18 +3,23 @@ package com.neet.GameState;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
 
+import javax.imageio.ImageIO;
+
+import com.neet.Handlers.Content;
 import com.neet.Handlers.Keys;
 import com.neet.Main.GamePanel;
 
 public class VictoryState extends GameState {
 
 	private Font font;
+	private BufferedImage victory;
 	
 	public VictoryState(GameStateManager gsm) {
 		super(gsm);
 		
-		font = new Font("Century Gothic", Font.PLAIN, 14);
+		font = new Font("Century Gothic", Font.PLAIN, 20);
 	}
 
 	public void init() {}
@@ -30,7 +35,15 @@ public class VictoryState extends GameState {
 		g.fillRect(0, 0, GamePanel.WIDTH, GamePanel.HEIGHT);
 		g.setColor(Color.WHITE);
 		g.setFont(font);
-		g.drawString("VICTORY", 90, 90);
+		g.drawString("YOU WIN!!!", 90, 90);
+		try {
+			BufferedImage Victory = ImageIO.read(getClass().getResourceAsStream("/Sprites/Other/victory.png"));
+			victory = Victory.getSubimage(0, 0, 270, 75);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		g.drawImage(victory, 100, 80, null );
+
 	}
 	
 	public void handleInput() {
